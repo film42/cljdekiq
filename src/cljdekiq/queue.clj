a3d6c564c84e800ee934463a2f652f9e62bfbda5
Garrett Thornburg <film42@gmail.com>
2025-04-01 01:11:55 -0600

    Transition all in-mem atom ops to swap!
    At this point the in-mem queue should not have any data races.

--- src/cljdekiq/queue.clj
+++ src/cljdekiq/queue.clj
@@ -18,45 +18,70 @@
         misses (or (get split false) [])]
     [hits, misses]))
 
+;; For retries and scheduled jobs, we use a map to create a redis sorted set.
+;; This lets us use swap! to rotate data, but also capture the value that was
+;; removed.
+(defn swap-pop-sorted-set! [atm at]
+  (let [res (atom nil)]
+    ;; Atomically split the collection to find the next value <= at.
+    (swap! atm (fn [coll]
+                 (let [[match rest] (split-by #(<= (:at %) at) coll)]
+                   ;; Store the match in a temporary atom.
+                   (reset! res match)
+
+                   rest)))
+
+    ;; Return the (maybe) found element.
+    @res))
+
+;; Take the main in-mem job queue, look to see if a job matches one of the
+;; provided queue names. The set of queues should be an actual set of strings.
+;; If a job is found, it will be captured and returned. Uses swap! to be thread
+;; safe.
+(defn swap-find-and-pop! [atm queues-str-set]
+  (let [res (atom nil)
+        job-not-in-queue-set (fn [j] (not (contains? queues-str-set
+                                                     (name (:queue j)))))]
+
+    ;; Atomically pop the first item matching one of the provided queues.
+    (swap! atm (fn [coll]
+                 ;; Sometimes times we get a vec out. Make sure we're always using a list.
+                 (let [coll-as-list (apply list coll)
+                       [skips [next & after]] (split-with job-not-in-queue-set coll-as-list)]
+
+                   ;; Capture the next job return before returning the remaining items to swap!.
+                   (reset! res next)
+
+                   (concat skips after))))
+
+    ;; Return the popped item.
+    @res))
+
 (defrecord TestQueue [jobs scheduled retry]
   Queue
 
-  ;; Not used.
   (tick [this]
-    (let [[to-enqueue1 retries] (split-by #(<= (:at %) (ct/now)) (deref (:retry this)))
-          [to-enqueue2 scheduled] (split-by #(<= (:at %) (ct/now)) (deref (:scheduled this)))]
-
-      ;; Update state
-      (reset! (:retry this) (into () retries))
-      (reset! (:scheduled this) (into () scheduled))
-
+    ;; Atomically pop jobs from sorted sets.
+    (let [to-enqueue1 (swap-pop-sorted-set! (:retry this) (ct/now))
+          to-enqueue2 (swap-pop-sorted-set! (:scheduled this) (ct/now))]
+      ;; Push the job if not nil.
       (doseq [{job :job} (concat to-enqueue1 to-enqueue2)]
         (push this job)))
 
-     ;; Sleep 1s
+    ;; Sleep 1s
     1)
 
-  ;; TODO: Make this ignore queues too.
   (poll [this queues]
-    ;; The list can turn lazy, so always transform back to list.
     (let [queue-str-set (set (map name queues))
-          jobs (apply list (deref (:jobs this)))
-          ;; Find the first job that matches one of the specified queues.
-          [skips [next-job & after]] (split-with
-                                      (fn [j] (not (contains? queue-str-set
-                                                              (name (:queue j)))))
-                                      jobs)
+          ;; Pop the next job that matches one of the provided queues.
+          next-job (swap-find-and-pop! (:jobs this) queue-str-set)
           ;; Make sure we convert the queue to a str.
           queue-name (if (map? next-job) (name (:queue next-job)))]
 
-      (if (some? next-job)
-        ;; We found a job. Pop the queue and return the rest.
-        (reset! (:jobs this) (concat skips after))
-
-        ;; Otherwise we'll just sleep to simulate brpop. Don't sleep long.
+      ;; If we don't have a job, simulate the redis brpop timeout.
+      (if (nil? next-job)
         (Thread/sleep 500))
 
-      ;; Return a [queue job] vec to match the spec.
       [queue-name next-job]))
 
   (push [this job]
@@ -65,22 +90,18 @@
     job)
 
   (retry [this job retry-at]
-    (let [retries (deref (:retry this))]
-      (reset! (:retry this)
-              (->>
-               (conj retries
-                     {:at retry-at :job job})
-               (sort-by :at))))
+    (swap! (:retry this) (fn [coll]
+                           (->>
+                            (conj coll {:at retry-at :job job})
+                            (sort-by :at))))
 
     job)
 
   (schedule [this job enqueue-at]
-    (let [scheduled (deref (:scheduled this))]
-      (reset! (:scheduled this)
-              (->>
-               (conj scheduled
-                     {:at enqueue-at :job job})
-               (sort-by :at))))
+    (swap! (:scheduled this) (fn [coll]
+                               (->>
+                                (conj coll {:at enqueue-at :job job})
+                                (sort-by :at))))
 
     job)
 
@@ -89,5 +110,3 @@
 (defn test-queue []
   ;; A bunch of atoms of empt lists for queue-like behavior.
   (->TestQueue (atom ()) (atom ()) (atom ())))
-
-
